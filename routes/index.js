b85202f7bbf1ea6d7bd18d64dfde0438c73e6c24
Renê Barbosa <renebarbosa@DEV-MC043.local>
2021-04-15 00:01:23 -0300

    init project

---
+++ routes/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+const ObjectsToCsv = require('objects-to-csv')
+
+var cheerio = require('cheerio');
+
+const ifoodUrl = "https://marketplace.ifood.com.br/v2/merchants?&channel=IFOOD&size=30&features=&categories=&payment_types=&delivery_fee_from=0&delivery_fee_to=25&delivery_time_from=0&delivery_time_to=240"
+const ifoodUrlDelivery = "https://www.ifood.com.br/delivery/";
+
+/* GET home page. */
+router.get('/', async function(req, res) {
+  var page = 0;
+  var pageTotal = 0;
+  var stores = [];
+  var promises = [];
+  console.log(`${ifoodUrl}&page=${page}&latitude=${req.query.latitude}&longitude=${req.query.longitude}`)
+
+  do {
+    var { data } = await axios.get(`${ifoodUrl}&page=${page}&latitude=${req.query.latitude}&longitude=${req.query.longitude}`);
+    if (pageTotal === 0) {
+      pageTotal = parseInt(data.total/data.size);
+    }
+    page++;
+    console.log(page, pageTotal)
+
+    promises.concat(data.merchants.map(async (store) => {
+      var dataHtml = { data } = await axios.get(`${ifoodUrlDelivery}/${store.slug}/${store.id}`);
+      var $ = cheerio.load(dataHtml.data)
+      var restaurantDetails = JSON.parse($('#__NEXT_DATA__').html()).props.initialState.restaurant.details;
+      
+      stores.push({
+        id: store.id,
+        name: store.name,
+        slug: store.slug,
+        userRating: store.userRating,
+        minimumOrderValue: store.minimumOrderValue,
+        distance: store.distance,
+        deliveryTime: store.deliveryTime,
+        district: restaurantDetails.address.district,
+        city: restaurantDetails.address.city,
+        state: restaurantDetails.address.state,
+        country: restaurantDetails.address.country,
+        latitude: restaurantDetails.address.latitude,
+        longitude: restaurantDetails.address.longitude,
+        timezone: restaurantDetails.address.timezone,
+        zipCode: restaurantDetails.address.zipCode,
+        streetName: restaurantDetails.address.streetName,
+        streetNumber: restaurantDetails.address.streetNumber,
+        cnpj: restaurantDetails.documents.CNPJ.value,
+      });
+    }));
+  } while (page < pageTotal);
+
+  await Promise.all(promises);
+  
+  const csv = new ObjectsToCsv(stores);
+  var fileName = `${req.query.latitude}X${req.query.longitude}.csv`;
+  await csv.toDisk(`./${fileName}`);
+
+  res.header('Content-Type', 'text/csv');
+  res.attachment(fileName);
+  res.sendFile(`./${fileName}`, { root: __dirname + '/..' });
+});
+
+module.exports = router;
